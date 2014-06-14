4276772a7677c859e3eecea759dcbcfb720f4f3e
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-06-13 20:15:21 -0500

    initial commit

---
+++ DurandalDemo/DurandalDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DurandalDemo.Startup))]
+namespace DurandalDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
